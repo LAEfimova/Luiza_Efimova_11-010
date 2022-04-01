3123efc76a11fa44302a241abefffc957dcb726e
Луиза Ефимова <81472682+LAEfimova@users.noreply.github.com>
2022-04-02 00:31:00 +0300

    2k-661

---
+++ WebCalculatorWithDI/CalcExpressionTreeBuilder/CalculatorVisitor.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+namespace WebCalculatorWithDI.CalcExpressionTreeBuilder
+{
+     public class CalculatorVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            var left = Task.Run(() => Visit(node.Left));
+            var right = Task.Run(() => Visit(node.Right));
+            
+            Task.WhenAll(left, right);
+            Thread.Sleep(1000);
+
+            var leftResult = (decimal)((ConstantExpression) left.Result)?.Value!;
+            var rightResult = (decimal)((ConstantExpression) right.Result)?.Value!;
+            
+            var operation = node.NodeType switch
+            {
+                ExpressionType.Add        => "+",
+                ExpressionType.Subtract   => "-",
+                ExpressionType.Multiply   => "*",
+                ExpressionType.Divide     => "/"
+            };
+
+            var result = (decimal)CalculatorF.Program.GetResult(new string[] {
+                leftResult.ToString(),
+                operation,
+                rightResult.ToString()})
+                .Value;
+        
+            return Expression.Constant(result);
+        }
+    }
+}
---
+++ WebCalculatorWithDI/Startup.cs
@@ -0,0 +1,38 @@
+﻿using WebCalculatorWithDI.CalcExpressionTreeBuilder;
+using WebCalculatorWithDI.Controllers;
+
+namespace WebCalculatorWithDI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IExpressionCalculator, ExpressionCalculator>();
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            _ = env.IsDevelopment()
+                ? app.UseDeveloperExceptionPage()
+                : app.UseExceptionHandler("/Home/Error").UseHsts();
+
+            app
+                .UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        "default",
+                        "{controller=Home}/{action=Index}/{id?}");
+                });
+        }
+    }
+}
